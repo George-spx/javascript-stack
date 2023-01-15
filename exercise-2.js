7dca5854e247f027d0131475233e52435313f5e4
georgev97 <georgeandrei.varga@gmail.com>
2023-01-15 12:01:38 +0100

    feat: change the if format using optional chaining

--- exercise-2.js
+++ exercise-2.js
@@ -1,18 +1,13 @@
 //In this example we are trying to check if a value for the `city` property is defined. However, the code can be simplified. How can we do that?
 
 const order = {
-  customer:{
+  customer: {
     address: {
-      city: "New York"
-    }
-  }
+      city: "New York",
+    },
+  },
 };
 
-if (
-  order &&
-  "customer" in order &&
-  "address" in order.customer &&
-  "city" in order.customer.address
-) {
+if (order?.customer?.address?.city === undefined) {
   console.log("City is required");
 }
