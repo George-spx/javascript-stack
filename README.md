aec807d8f7ee65ee852b43842b0e8ecf7f72f414
georgev97 <georgeandrei.varga@gmail.com>
2023-01-15 11:47:22 +0100

    feat: add function multiplyByTwo

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Exercise Closures
 
-Try to write your first `closure`. Write a function called `printName` and declare inside it a variable called `helloName`, the value of this variable will be: `"Hello John"`.
-Also declare a function named `inner` and return the `helloName` variable.
-The purpose is printing `"Hello John"` in the console.
+Write a new `closure`: create a function called `multiplyByTwo` that takes in one parameter.
+Inside the `multiplyByTwo` function, create a function called `inner`. This function will take care of multiplying the parameter passed as input and multiply it by the variable named `number`
+Print in the console the function output.
+
+Example: `multiplyByTwo(4)()`, result: `8`
 
 Tips:
-- You can learn more about this topic checking the following article: https://www.freecodecamp.org/news/closures-in-javascript/
+
+- You can learn more about this topic checking this article: https://www.freecodecamp.org/news/closures-in-javascript/
--- exercise.js
+++ exercise.js
@@ -1,13 +1,15 @@
-/*Try to write your first `closure`. Write a function called `printName` and declare inside it a variable called `helloName`, the value of this variable will be: `"Hello John"`.
-Also declare a function named `inner` and return the `helloName` variable.
-The purpose is printing `"Hello John"` in the console. */
+/**Write a new `closure`: create a function called `multiplyByTwo` that takes in one parameter.
+Inside the `multiplyByTwo` function, create a function called `inner`. This function will take care of multiplying the parameter passed as input and multiply it by the variable named `number`
+Print in the console the function output.
 
-function printName(name) {
-    let helloName = "Hello "+name;
-    function inner() {
-        return helloName;
-    }
-    return inner();
+Example: `multiplyByTwo(4)()`, result: `8` */
+
+function multiplyByTwo(value) {
+  let number = 2;
+  function inner() {
+    return value * number;
+  }
+  return inner();
 }
 
-console.log(printName("John"));
+console.log(multiplyByTwo(4));
