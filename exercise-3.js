8c196f9c3cf3678ad4ec15a5876cd25f4cc97251
georgev97 <georgeandrei.varga@gmail.com>
2023-01-21 12:00:10 +0100

    feat: add fromObject method

---
+++ exercise-3.js
@@ -0,0 +1,20 @@
+/*Define inside the class `Person` a static method called `fromObject` that takes in an object literal as parameter and instantiate a `Person` object.*/
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  static fromObject(obj) {
+    return new Person(obj.firstName, obj.lastName);
+  }
+}
+
+const obj = {
+  firstName: "Mario",
+  lastName: "Rossi",
+};
+
+const person = Person.fromObject(obj);
+console.log(person.firstName + " " + person.lastName);
