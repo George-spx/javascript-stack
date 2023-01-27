eae44c81cc1e063ad4c6bf538c7b46867afe5a70
georgev97 <georgeandrei.varga@gmail.com>
2023-01-27 21:44:28 +0100

    feat: refactor code using string template

---
+++ exercise.js
@@ -0,0 +1,14 @@
+//Perform a code refactoring by using the Template Literals.
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+const developer = new Person("Mario", "Rossi");
+console.log(developer.firstName + " " + developer.lastName);
+
+//refactored console.log
+console.log(`${developer.firstName} ${developer.lastName}`);
