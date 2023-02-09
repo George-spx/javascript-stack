cf1826d616af92ef603772d6631595f159dc3ae7
georgev97 <georgeandrei.varga@gmail.com>
2023-02-09 08:30:14 +0100

    feat method printAsyncName

---
+++ exercise.js
@@ -0,0 +1,9 @@
+/*Write a function called `printAsyncName`, which will take as parameters a callback and a string (which will be name). The callback function will simply print "Hello".
+The `printAsyncName` function will have to execute the callback function after an interval of 1 second. After an interval of 2 seconds, we must print the name that we take as a parameter.*/
+
+function printAsyncName(callback, name) {
+  setTimeout(callback, 1000);
+  setTimeout(() => console.log(name), 2000);
+}
+
+printAsyncName(() => console.log("Hello"), "John");
